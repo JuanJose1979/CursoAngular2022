bda326e0900d43c5c9c0904688e17c8de010534a
Juan Jose Rojas <juanjoserojas@msn.com>
2023-06-05 09:43:36 -0600

    version-1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'MiprimerApps angular';
+
+  public ArregloTarjetas: Tarjeta[]=[];
+
+  ngOnInit(): void {  
+      this.ArregloTarjetas = [
+        {titulo: 'video 1', subtitulo:'Subtitulo video 1'},
+        {titulo: 'video 2', subtitulo:'subtitulo video 2'},
+        {titulo: 'video 3', subtitulo:'subtitulo video 3'},
+        {titulo: 'video 3', subtitulo:'subtitulo video 4'},
+      ]
+  }
+}
